e9ca14cadfe28416bbe5e7afc89c0a84e36b912c
Juan Rey <sudo.delete.evil@gmail.com>
2026-03-24 11:33:53 -0500

    update : desktop index page changes.

--- backend/services/web-dashboard/app/pages/index.vue
+++ backend/services/web-dashboard/app/pages/index.vue
@@ -1,28 +1,82 @@
 <script setup lang="ts">
-import type { ContextMenuItem } from '@nuxt/ui'
+import type { ContextMenuItem } from "@nuxt/ui";
+import type { BreadcrumbItem } from "@nuxt/ui";
 
-const items: ContextMenuItem[][] = [
+const breadItems: BreadcrumbItem[] = [
+  {
+    label: "Home",
+    icon: "i-lucide-home",
+    to: "/",
+  },
+];
+
+const contextItems: ContextMenuItem[][] = [
   [
     {
-      label: 'File upload',
-      icon: 'i-lucide-file-up'
+      label: "New folder",
+      icon: "i-lucide-folder-plus",
+    },
+  ],
+  [
+    {
+      label: "File upload",
+      icon: "i-lucide-file-up",
     },
     {
-      label: 'Folder upload',
-      icon: 'i-lucide-folder-up'
-    }
-  ]
-]
+      label: "Folder upload",
+      icon: "i-lucide-folder-up",
+    },
+  ],
+];
 </script>
 
 <template>
-  <UContextMenu :items="items" :ui="{ content: 'w-60' }">
-    <div class="flex items-center justify-center rounded-md border border-dashed border-accented text-sm aspect-video w-200">
-    
-
-      
-
+  <div class="index-page">
+    <div class="index-page-header">
+      <UBreadcrumb
+        :items="breadItems"
+        :ui="{
+          link: 'group relative flex items-center gap-1.5 text-base min-w-0 focus-visible:outline-primary',
+        }"
+      >
+        <template #separator>
+          <span class="mx-2 text-muted">/</span>
+        </template>
+      </UBreadcrumb>
     </div>
-  </UContextMenu>
+
+    <UContextMenu :items="contextItems" :ui="{ content: 'w-60' }">
+      <div class="index-page-content">
+        <UEmpty
+          icon="i-lucide-file"
+          title="No projects found"
+          description="It looks like you haven't added any projects. Create one to get started."
+        />
+      </div>
+    </UContextMenu>
+  </div>
 </template>
 
+<style lang="css" scoped>
+.index-page {
+  flex-direction: column;
+  display: flex;
+  height: 100%;
+  width: 100%;
+}
+
+.index-page-header {
+  align-items: center;
+  padding: 1rem;
+  display: flex;
+  height: 3rem;
+}
+
+.index-page-content {
+  justify-content: center;
+  align-items: center;
+  height: inherit;
+  width: inherit;
+  display: flex;
+}
+</style>
