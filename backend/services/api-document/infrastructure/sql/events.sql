efb6841fef7f201b66a8a1afcc5dc957ef5d3038
Juan Rey <sudo.delete.evil@gmail.com>
2026-03-28 17:57:04 -0500

    Update events.sql

--- backend/services/api-document/infrastructure/sql/events.sql
+++ backend/services/api-document/infrastructure/sql/events.sql
@@ -1,16 +1,17 @@
 CREATE TABLE events (
-    id UUID PRIMARY KEY, 
-    type           VARCHAR(255) NOT NULL,  
-    source         VARCHAR(255) NOT NULL,         
-    aggregate_type VARCHAR(255) NOT NULL,             
-    aggregate_id   VARCHAR(255) NOT NULL,                           
-    data        JSONB           NOT NULL,               
-    metadata       JSONB        NOT NULL DEFAULT '{}',
-    time           BIGINT       DEFAULT NULL            
-    specversion    BIGINT       NOT NULL
+    specversion    BIGINT       NOT NULL,
+    type           VARCHAR(255) NOT NULL,     
+    source         VARCHAR(255) NOT NULL,      
+    id             UUID         PRIMARY KEY,      
+    time           BIGINT       DEFAULT NULL,
+    entity_type    VARCHAR(255) NOT NULL,             
+    entity_id      VARCHAR(255) NOT NULL,                           
+    data           JSONB        NOT NULL,
+
+    metadata       JSONB        NOT NULL DEFAULT '{}'
 );
 
 CREATE INDEX idx_events_created_at     ON events (created_at);
-CREATE INDEX idx_events_aggregate      ON events (aggregate_type, aggregate_id);
+CREATE INDEX idx_events_aggregate      ON events (entity_type, entity_id);
 CREATE INDEX idx_events_type           ON events (type, created_at);
 
